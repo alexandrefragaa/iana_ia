3bbe55b5135f4e7208d5256e55b5c7253248cccb
0Al3x <59896657+alexandrefragaa@users.noreply.github.com>
2026-06-26 09:46:04 -0300

    add crawler and schema

---
+++ crawler/savetoDb.js
@@ -0,0 +1,10 @@
+//salvar no banco de dados
+
+import { db } from "../backend-node/src/db/mysql.js";
+
+export function saveToDB(title, link) {
+    db.query(
+        "INSERT INTO knowledge (title, url) VALUES (?, ?)",
+        [title, link]
+    );
+}
